a6fb9459e0058a297ace46060b3f47eed2a93b09
Shio <154671616+SunDice0@users.noreply.github.com>
2024-02-03 11:06:43 +0200

    Update main_window.py

--- Memory_card/main_window.py
+++ Memory_card/main_window.py
@@ -1,89 +1,82 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import*
-
-
-
-def switch_screen():
-    if btn_answer.text()=="Відповістии":
-        qwestion_group.hide()
-        answer_group.show()
-        btn_answer.setText("Наступне питання")
-    else:
-        qwestion_group.show()
-        answer_group.hide()
-        btn_answer.setText("Відповісти")
-
-btn_menu = QPushButton("Meню")
-btn_sleep = QPushButton("Відпочити")
-btn_answer = QPushButton("Відповісти")
-timer=QSpinBox()
-timer.setValue(30)
-timer_text = QLabel("хвилин")
-
-text_qwestion = QLabel("Питання")
-right_answer = QLabel("Відповідь")
-text_result = QLabel("Результат")
-
-rbtn1=QRadioButton("1")
-rbtn2=QRadioButton("2")
-rbtn3=QRadioButton("3")
-rbtn4=QRadioButton("4")
-
-radiogroup = QButtonGroup()
-radiogroup.addButton(rbtn1)
-radiogroup.addButton(rbtn2)
-radiogroup.addButton(rbtn3)
-radiogroup.addButton(rbtn4)
-
-qwestion_group=QGroupBox("Варіанти відповідей")
-answer_group=QGroupBox("Пезультат тесту")
-
-line=QVBoxLayout()
-line1=QHBoxLayout()
-line2=QHBoxLayout()
-line3=QHBoxLayout()
-line3_q=QHBoxLayout()
-line3_a=QVBoxLayout()
-line4=QHBoxLayout()
-
-line_v1=QVBoxLayout()
-line_v2=QVBoxLayout()
-
-line_v1.addWidget(rbtn1)
-line_v1.addWidget(rbtn2)
-line_v2.addWidget(rbtn3)
-line_v2.addWidget(rbtn4)
-
-line3_q.addLayout(line_v1)
-line3_q.addLayout(line_v2)
-
-qwestion_group.setLayout(line3_q)
-
-line3_a.addWidget(text_result,alignment=(Qt.AlignLeft |Qt.AlignTop))
-line3_a.addWidget(right_answer,alignment=Qt.AlignCenter)
-answer_group.setLayout(line3_a)
-answer_group.hide()
-
-line1.addWidget(btn_menu)
-line1.addStretch(1)
-line1.addWidget(btn_sleep)
-line1.addWidget(timer)
-line1.addWidget(timer_text)
-
-line2.addWidget(text_qwestion,alignment=Qt.AlignCenter)
-
-line3.addWidget(qwestion_group)
-line3.addWidget(answer_group)
-
-line4.addStretch(1)
-line4.addWidget(btn_answer,stretch=2)
-line4.addStretch(1)
-
-line.addLayout(line1,stretch=1)
-line.addLayout(line2,stretch=2)
-line.addLayout(line3,stretch=6)
+from PyQt5.QtCore import Qt 
+from PyQt5.QtWidgets import* 
+ 
+ 
+ 
+ 
+ 
+btn_menu = QPushButton("Meню") 
+btn_sleep = QPushButton("Відпочити") 
+btn_answer = QPushButton("Відповісти") 
+timer=QSpinBox() 
+timer.setValue(30) 
+timer_text = QLabel("хвилин") 
+ 
+text_qwestion = QLabel("Питання") 
+right_answer = QLabel("Відповідь") 
+text_result = QLabel("Результат") 
+ 
+rbtn1=QRadioButton("1") 
+rbtn2=QRadioButton("2") 
+rbtn3=QRadioButton("3") 
+rbtn4=QRadioButton("4") 
+ 
+radiogroup = QButtonGroup() 
+radiogroup.addButton(rbtn1) 
+radiogroup.addButton(rbtn2) 
+radiogroup.addButton(rbtn3) 
+radiogroup.addButton(rbtn4) 
+ 
+qwestion_group=QGroupBox("Варіанти відповідей") 
+answer_group=QGroupBox("Результат тесту") 
+ 
+line=QVBoxLayout() 
+line1=QHBoxLayout() 
+line2=QHBoxLayout() 
+line3=QHBoxLayout() 
+line3_q=QHBoxLayout() 
+line3_a=QVBoxLayout() 
+line4=QHBoxLayout() 
+ 
+line_v1=QVBoxLayout() 
+line_v2=QVBoxLayout() 
+ 
+line_v1.addWidget(rbtn1) 
+line_v1.addWidget(rbtn2) 
+line_v2.addWidget(rbtn3) 
+line_v2.addWidget(rbtn4) 
+ 
+line3_q.addLayout(line_v1) 
+line3_q.addLayout(line_v2) 
+ 
+qwestion_group.setLayout(line3_q) 
+ 
+line3_a.addWidget(text_result,alignment=(Qt.AlignLeft |Qt.AlignTop)) 
+line3_a.addWidget(right_answer,alignment=Qt.AlignCenter) 
+answer_group.setLayout(line3_a) 
+answer_group.hide() 
+ 
+line1.addWidget(btn_menu) 
+line1.addStretch(1) 
+line1.addWidget(btn_sleep) 
+line1.addWidget(timer) 
+line1.addWidget(timer_text) 
+ 
+line2.addWidget(text_qwestion,alignment=Qt.AlignCenter) 
+ 
+line3.addWidget(qwestion_group) 
+line3.addWidget(answer_group) 
+ 
+line4.addStretch(1) 
+line4.addWidget(btn_answer,stretch=2) 
+line4.addStretch(1) 
+ 
+line.addLayout(line1,stretch=1) 
+line.addLayout(line2,stretch=2) 
+line.addLayout(line3,stretch=6) 
+line.addStretch(1) 
+line.addLayout(line4,stretch=3) 
 line.addStretch(1)
-line.addLayout(line4,stretch=3)
 line.addStretch(1)
 
 
